e3e92b4142e95bc5881b2f94415b10bb7a7c9d6a
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-12-17 11:10:45 +0100

    AB25 & UB11

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const log4js = require('log4js')
-const http = require('http')
 
-const config = require('dotenv-extended').load({
-    schema: '.env.schema',
-    errorOnMissing: true,
-    errorOnExtra: true
-})
+const log4js = require('log4js');
+const dotenv = require('dotenv-extended');
+const express = require('express')
 
+log4js.configure('log4js.json');
+let logger = log4js.getLogger('App');
 
-const logger = log4js.getLogger('App');
-logger.level = 'info'
+// Read the properties from file '.env' and '.env.defaults'
+dotenv.load({silent: true});
+let PORT = process.env.PORT || 8000;
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain'})
-    response.end('Hello World!\n')
-})
+var app = express()
 
-server.listen(config.PORT)
-logger.info(`Server is running on port ${config.PORT}`)
+app.get('/', (req, res) =>
+  res.send('Hello World\n')
+)
+app.listen(PORT)
+
+// Put a friendly message on the terminal
+logger.info(`Server running on ${PORT}`);
