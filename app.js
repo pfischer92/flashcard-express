dbe939409d73bdbb8b3f705309720fbd131bfe56
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-12-17 11:32:39 +0100

    AB26

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
+"use strict";
 
 const log4js = require('log4js');
 const dotenv = require('dotenv-extended');
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const Questionnaire = require('./domain/questionnaire');
 
-log4js.configure('log4js.json');
-let logger = log4js.getLogger('App');
 
 // Read the properties from file '.env' and '.env.defaults'
 dotenv.load({silent: true});
-let PORT = process.env.PORT || 8000;
 
-var app = express()
+// Configure log4js based on the definitions in file 'log4js.json'
+log4js.configure('log4js.json');
+const logger = log4js.getLogger('app');
 
-app.get('/', (req, res) =>
-  res.send('Hello World\n')
-)
-app.listen(PORT)
+// Use native promises, needed with Mongoose since v4.1.0
+mongoose.Promise = global.Promise;
 
-// Put a friendly message on the terminal
-logger.info(`Server running on ${PORT}`);
+// Connect to the database using the connection parameters found in the property-files
+const url = 'mongodb://' + process.env.MONGO_HOST + '/' + process.env.MONGO_DATABASE;
+logger.debug(`Database URL used "${url}"`);
+mongoose.connect(url, {
+    useMongoClient: true
+});
+
+// Create the Express Server App
+const app = express();
+
+// Read PORT from the configuration, default to 8000
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    Questionnaire.find((err, questionnaires) => {
+        if (err) {
+          return res.status(400).send('database error');
+        }
+        logger.debug(`Found ${questionnaires.length} questionnaires`);
+        res.status(200).json(questionnaires);
+      });
+
+    logger.debug("Successfully processed %s request for '%s'", req.method, req.url);
+  })
+
+  
+// Start the App as HTTP server
+app.listen(PORT);
+
+// Use backquotes for the es6 feature
+logger.info(`Server started on port ${PORT}`);
+
+module.exports = app;
